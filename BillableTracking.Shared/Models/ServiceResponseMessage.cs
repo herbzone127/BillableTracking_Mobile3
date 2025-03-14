a1a323b5f53ac0d605f4a04f0fe335fdb18c0610
herbzone127 <38952209+herbzone127@users.noreply.github.com>
2025-03-14 22:53:21 +0500

    Delete BillableTracking.Shared directory

--- BillableTracking.Shared/Models/ServiceResponseMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BillableTracking.Shared.Models
-{
-    public class ServiceResponseMessage<T>
-    {
-        public T? Data { get; set; }
-        public string Message { get; set; } = "";
-        public int StatusCode { get; set; }
-    }
-}
